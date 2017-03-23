3aecb7da60da357a2d20395dc83977489c2eabff
Jean <jean.sordes@yahoo.fr>
2017-03-23 16:48:31 +0100

    initial commit

---
+++ src/index.php
@@ -0,0 +1,44 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Create and configure Slim app
+$app = new \Slim\App(['settings' => [ 'addContentLengthHeader' => false]]);
+
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/templates', [
+        'cache' => __DIR__ . '/templates/cache'
+    ]);
+    
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
+
+//Override the default Not Found Handler
+$container['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        $c->view->render($response, 'error.html', [ 'message' => '404 - page not found' ]);
+        return $c['response']
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'text/html');            
+    };
+};
+
+
+// Define app routes
+$app->get('/[hello/{name}]', function ($request, $response, $args) {
+    $title = empty($args['name']) ? 'you' : $args['name'] ;
+    return $this->view->render($response, 'homepage.html', [
+        'title' => 'Hello ' . $title,
+        'body' => 'And welcome!'
+    ]);
+})->setName('homepage');
+
+// Run app
+$app->run();
