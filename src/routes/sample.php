060b7f9e053eaef2b684b20cc35f0fe60d65f95f
Jean <jean.sordes@yahoo.fr>
2018-09-02 23:01:41 +0200

    template improved

--- src/routes/sample.php
+++ src/routes/sample.php
@@ -2,14 +2,14 @@
 $app->get('/[hello/{name:[A-Za-z]*}]', function ($request, $response, $args) {
     $title = empty($args['name']) ? 'you' : $args['name'];
     return $this->view->render('homepage.html.twig', [
-        'title' => 'Hello ' . $title,
-        'body' => "And welcome! Here is a random string : " . MyApp\Utility\Math::getARandomString(),
+        'title' => "Hello $title !",
+        'body' => "And welcome ! Here is a random string : " . MyApp\Utility\Math::getARandomString(),
     ]);
 });
 
 $app->get('/zipcode/{code:[0-9]{5}}', function ($request, $response, $args) {
     return $this->view->render('homepage.html.twig', [
-        'title' => 'Hello you!',
+        'title' => 'Hello you !',
         'body' => "You entered the following zipcode : " . $args["code"],
     ]);
 });
